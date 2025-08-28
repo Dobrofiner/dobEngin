d83de67b3884c4268de62368b9850f26ed694e97
Dobrofiner <121291587+Dobrofiner@users.noreply.github.com>
2025-08-28 16:04:50 +0700

    Обнова 0.5.0, 0.5.0bLvLLoader,Mouse,fitConfigEdition

--- cameraTest.js
+++ cameraTest.js
@@ -1,5 +1,6 @@
 let engin = new dobEngin;
 engin.start();
+engin.curLevel.isInfinite="true"
 engin.assets = ["./assets/player.png"];
 engin.placeAssetsImgs();
 engin.createObjectType("sprite","player","player",1,1,"player.png");
@@ -10,16 +11,34 @@ engin.spawnObject("text1","text1",1,100,"the sad yes")
 engin.spawnObject("text1","text3",1000,1000,"the sad yes")
 engin.camera.follow = "sad";
 addEventListener("keydown",(event)=>{
-    if (event.key = "w") {engin.objects[0].props.y += 1}
-   else if (event.key = "a") {engin.objects[0].props.x -= 1}
-   else if (event.key = "s") {engin.objects[0].props.y -= 1}
-   else if (event.key = "d") {engin.objects[0].props.x += 1}
+    let key = event.key
+    if (key == "w"||key=="ц" || key =='W' || key=='Ц') {engin.objects[0].props.y -= 1;}
+    if (key == "a"||key == "ф"||key == "A"||key == "Ф") {engin.objects[0].props.x -= 1;}
+    if (key == "s"||key == "ы"||key == "S"||key == "Ы") {engin.objects[0].props.y += 1;}
+    if (key == "d"||key == "в"||key == "D"||key == "В") {engin.objects[0].props.x += 1;}
+    else{}
+    console.log(key)
 })
+addEventListener("keypress",(event)=>{
+    console.log(event.key)
+})
+engin.createObjectType("sprite","mouse","mouse",640/2,480/2,100,100)
+const mouse = engin.spawnObject("mouse","mouse",640/2,480/2,"player.png",10,10)
+
 engin.setCameraCoords(engin.objects[0])
 engin.gameLoop  = () => {
+    
+    engin.drw.fillText(`Mouse x:${engin.mouse.x}
+        Mouse y:${engin.mouse.y}`,100,10)
+        mouse.props.x = engin.mouse.x
+        mouse.props.y = engin.mouse.y
     engin.drw.fillText(Math.random()*10,0+100,10+100,1000)
     if (engin.fps % 60)engin.drw.fillText(`FPS: ~${engin.fps}`, 10+100, 20+100);
     engin.setCameraCoords(engin.objects[0].props.x,engin.objects[0].props.y)
 engin.renderCamera(engin.objects[0]);
 }
+engin.canvas.addEventListener("click",()=>{
+    const [x,y]= [engin.mouse.x,engin.mouse.y]
+    engin.spawnObject("player","plr",x,y)
+})
 engin.startGameLoop()
--- dobengin.js
+++ dobengin.js
@@ -1,8 +1,24 @@
+/**
+ * dobEngin v0.5.0<br>
+ * 
+ * License:MIT
+ * 
+ * By Dobrofiner
+ */
 class dobEngin {
    
-    constructor() {
+    constructor(usePreCreatedCanvas=false,canvasId='') {
         //creating global vars of engine;
-        this.config = {};
+        this.config = {
+            canvasColor:"#fff",
+            bodyColor:"#000",
+            physicalCanvasHeight:'100%',
+            physicalCanvasWidth:"100%",
+            physGameDivHeight:'95vh',
+            physGameDivWidth:'75%',
+            bodyJustifyContentType:"center",
+            bodyDisplay:"flex"
+        };
         this.objects = [];
         this.objectsTypes = {};
         this.isStarted = false;
@@ -22,17 +38,68 @@ class dobEngin {
             "width":this.width,
             "follow":"nothing"
         }
+        this.curLevel = {
+            width:640,
+            height:480
+        }
+        this.mouse = {
+            x:0,
+            y:0,
+            realX:0,
+            realY:0
+        }
+        this.physicalScaleX=1
+        this.physicalScaleY=1
         this.animations = {}
+        this.canvas = document.createElement("canvas");
+
+        this.normalizeCoordsWithScale=(x,y)=>{
+            x=parseFloat(x);
+            y=parseFloat(y)
+            return [x*this.physicalScaleX,y*this.physicalScaleY]
+        }
+        this.mouseToCanvasXY=(mousX,mousY)=>{
+            mousX = parseFloat(mousX);
+            mousY = parseFloat(mousY);
+            return [this.camera.x+mousX,this.camera.y+mousY]
+        }
+        this.updateScales = () => {
+            this.physicalScaleX = this.width / this.canvas.clientWidth;
+           this.physicalScaleY = this.height / this.canvas.clientHeight;
+   };
         //creatung canvas and setting it to work
         this.start = () => {
             if (!this.isStarted){
+                if(document.getElementById(canvasId) == null && usePreCreatedCanvas){
+                    canvasId='';throw new Error("Канваса не существует!Создаем свой...")
+                }
+                if(!usePreCreatedCanvas && (canvasId == null || canvasId.trim()=='')){
             this.canvas = document.createElement("canvas");
             this.canvas.height = this.height;
             this.canvas.width = this.width;
             this.drw = this.canvas.getContext("2d");
             document.body.append(this.gameDiv);
+            document.body.style.backgroundColor=this.config.bodyColor
+            document.body.style.display=this.config.bodyDisplay
+            document.body.style.justifyContent=this.config.bodyJustifyContentType
+            this.gameDiv.style.width=this.config.physGameDivWidth
+            this.gameDiv.style.height=this.config.physGameDivHeight
             this.gameDiv.appendChild(this.canvas);
+                        this.canvas.style.width=this.config.physicalCanvasWidth;
+            this.canvas.style.height=this.config.physicalCanvasHeight
+            this.canvas.style.backgroundColor=this.config.canvasColor
             this.isStarted = true;}
+            else{
+                this.canvas = document.getElementById(canvasId);
+                this.width = this.canvas.width; 
+                this.height = this.canvas.height;
+                // this.drw = this.canvas.getContext("2d");
+            }
+            const rect = this.canvas.getBoundingClientRect()
+            this.canvas.addEventListener("mousemove",(event)=>{
+                [this.mouse.realX,this.mouse.realY]=[event.clientX-rect.left,event.clientY-rect.top];    
+            })
+        }
         }
         this.placeAssetsImgs = () =>{
     this.assets.forEach(el=>{
@@ -46,6 +113,8 @@ class dobEngin {
         clearInterval(this.updateInterval);
        this.updateInterval = setInterval(() => {
             this.drw.clearRect(0,0,this.width,this.height)
+            this.updateScales();
+            [this.mouse.x,this.mouse.y]=this.mouseToCanvasXY(...this.normalizeCoordsWithScale(this.mouse.realX,this.mouse.realY))    
             this.gameLoop();
             this.fps++;
         },1000/this.fps)
@@ -63,7 +132,13 @@ class dobEngin {
                              return spr }
                 }  
                 this.getAnimByName=(anim)=>{
-this.props.animations[anim] = engine.animations[anim] || null
+                    if(anim.trim()==""||anim.length==0)throw new Error(`Ничего не введено.`)
+                    if(engine.animations[anim]==undefined){
+                        throw new Error(`Анимация ${anim} не существует. Анимация не получена`)
+                    }
+                    else {
+                        this.props.animations[anim] = engine.animations[anim] || null
+                    }
                 }
                     this.propsDef = {
                         "name":properties[0],
@@ -121,13 +196,6 @@ if(this.curAnim.curFrame == animation.animArr.length-1 && cycle == true) this.cu
                 }
                 this.type="sprite";
                 this.dUid = 0;
-                this.setProps = (properties) =>{
-                    for (let i=0;i<this.props.length;i++){
-                        if (properties[i] !== undefined) {  
-                            this.props[Object.keys(this.props)[i]] = properties[i];  
-                }
-            }
-        }
             }
         }
         this.text = class text {
@@ -148,13 +216,6 @@ if(this.curAnim.curFrame == animation.animArr.length-1 && cycle == true) this.cu
             } 
             this.type = "text";
             this.dUid = 0;
-            this.setProps = (properties) =>{
-                        for (let i=0;i<this.props.length;i++){
-                            if (properties[i] !== undefined) {  
-                                this.props[Object.keys(this.props)[i]] = properties[i];  
-                    }
-                }
-            }
             this.typeInterval = 0;
             this.typewriter = (text="",length=0) =>{
                 let oneSymTime = length/text.length;
@@ -197,6 +258,7 @@ if(this.curAnim.curFrame == animation.animArr.length-1 && cycle == true) this.cu
 
                 this.objects.push(newObj);
                 console.log(`created object.type ${type}.duid on data: ${newObj.dUid}`);
+                return newObj;
             }
           /*   this.objects[id].dUid = this.dUid(); */
         }
@@ -231,21 +293,38 @@ if(this.curAnim.curFrame == animation.animArr.length-1 && cycle == true) this.cu
     this.renderCamera = (objectFollow) => {
         this.setCameraCoords(objectFollow.props.x,objectFollow.props.y)
         this.objects.forEach(el=>{
-            if (el.props.x + el.props.width > this.camera.x && 
-                el.props.x < this.camera.x + this.camera.width &&  
-                el.props.y + el.props.height > this.camera.y && 
-                el.props.y < this.camera.y + this.camera.height){
-               if(el.type=="sprite"){
-                if(el.dUid == objectFollow.dUid){
-                    this.drw.drawImage(el.props.spriteImage,this.camera.width/2,this.camera.height/2,el.props.width,el.props.height);
+            if(!this.curLevel.isInfinite && 
+            (el.props.x + el.props.width< 0 || el.props.x>this.curLevel.width
+            || el.props.y+el.props.height < 0 || el.props.y > this.curLevel.height)){}
+            else{
+                if (el.props.x + el.props.width > this.camera.x && 
+                    el.props.x < this.camera.x + this.camera.width &&  
+                    el.props.y + el.props.height > this.camera.y && 
+                    el.props.y < this.camera.y + this.camera.height){
+                   if(el.type=="sprite"){
+                    
+                    const centerX = (this.camera.width - el.props.width) / 2;
+                    const centerY =(this.camera.height - el.props.height) / 2;
+                    
+                    if(el.props.spriteImage instanceof(Image)){
+                    if(el.dUid == objectFollow.dUid){
+                        this.drw.drawImage(el.props.spriteImage,centerX,centerY,el.props.width,el.props.height);
+                    }else{
+                        this.drw.drawImage(el.props.spriteImage,el.props.x-this.camera.x,el.props.y-this.camera.y,el.props.width,el.props.height); 
+                    }
                 }else{
-                    this.drw.drawImage(el.props.spriteImage,el.props.x-engin.camera.x,el.props.y-engin.camera.y,el.props.width,el.props.height); 
+                    if(el.dUid == objectFollow.dUid){
+                        this.drw.fillRect(centerX,centerY,el.props.width,el.props.height);
+                    }else{
+                        this.drw.fillRect(el.props.x-this.camera.x,el.props.y-this.camera.y,el.props.width,el.props.height); 
+                    }
                 }
-           }else if(el.type =="text"){
-              this.drw.fillText(el.props.text,el.props.x-engin.camera.x,el.props.y-engin.camera.y)
-           }
+               }else if(el.type =="text"){
+                  this.drw.fillText(el.props.text,el.props.x-this.camera.x,el.props.y-this.camera.y)
+               }
+                }
             }
-         })
+       })
     }
     this.addAnim=(arr=[],name=Math.round((Math.random()*10)*100)/100,isGif = false)=>{
         if(!this.isArr(arr)) return "not array. Error: STPD ERROR 197";
@@ -261,5 +340,53 @@ this.isArr=arr=>{try{arr.reverse();return true;}catch(e){return false}};
     this.stop=()=>{clearInterval(this.updateInterval);this.isStarted = false;};
     this.resume=()=>{if(!this.isStarted)this.startGameLoop()};
     this.setFps=(fps)=>{this.stop();this.fps = fps;this.resume()};
+    this.levelLoader = class levelLoader{
+        constructor(engine){
+this.engine = engine;
         }
+        loadLevel(lvl){
+let xhr = new XMLHttpRequest;
+xhr.open("GET",lvl,true);
+xhr.onload=()=>{
+    if(xhr.status == 200) {
+    this.engine.curLevel = JSON.parse(xhr.responseText);
+    this.engine.objects = [];
+    this.parseLevel(this.engine.curLevel)}
+};
+xhr.send()
+        }
+     getSprite(spr){
+            if(typeof(spr) == "string"){
+                return this.engine.assetsForUse[spr]
+            }else{
+                return spr }
+        }  
+        parseLevel(){
+if(this.engine.curLevel.isdobEnginLv == false){throw new Error("Это не уровень dobEngin!");}
+Object.assign(this.engine.camera,this.engine.curLevel.camera)
+this.engine.nextDuid = 0;
+for (const obj in this.engine.curLevel.objects){
+    const objN =  this.engine.curLevel.objects[obj];
+/*     console.log(objN) */
+    this.spawnObj(objN.objectType,objN.props);
+}
+        }
+        spawnObj(type,propsObj){
+let arr = new Array(propsObj.length).fill(0);
+let obj = this.engine.spawnObject(type,arr);
+Object.assign(obj.props,propsObj)
+if(obj.type == "sprite" && typeof(obj.props.spriteImage) == "string"){
+    obj.props.spriteImage =this.getSprite(obj.props.spriteImage)
+        }
+if(obj.type == "sprite" && Array.isArray(obj.props.animations)){
+    let anims = {}
+    obj.props.animations.forEach(el=>{
+        anims[el] = obj.getAnimByName(el)
+    })
+    obj.props.animations = anims;
+        }
+    }
+    }
+    this.lvlLoader = new this.levelLoader(this);
+}
     }
